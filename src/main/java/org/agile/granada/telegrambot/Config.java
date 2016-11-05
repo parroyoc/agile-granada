873680cc0e986eb0fd450ba4d33cef3563004c2a
Pablo <parroyoc@gmail.com>
2016-11-05 16:47:10 +0000

    fixing config

--- src/main/java/org/agile/granada/telegrambot/Config.java
+++ src/main/java/org/agile/granada/telegrambot/Config.java
@@ -2,10 +2,10 @@ package org.agile.granada.telegrambot;
 
 public class Config {
     public static String getBotUsername() {
-    	return "AgileGranada0_bot"; //System.getenv("BOT_USERNAME");
+    	return System.getenv("BOT_USERNAME");
     }
 
     public static String getBotToken() {
-    	return "276518186:AAGk1DsJy3Ef0IlmZLoXKnwQKOrtwf5BzNs";// System.getenv("BOT_TOKEN");
+    	return System.getenv("BOT_TOKEN");
     }
 }
